cc150572ddb6357f6eed79733e0e47cd2d56cac7
DoomLaptop <umutw4141@gmail.com>
2025-09-30 17:34:31 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React + Vite Template
+
+## 📂 Installation & Usage  
+```bash
+# clone repository
+git clone https://github.com/umutfr/React-TailwindCSS-Router-Template.git
+
+# install dependencies
+npm install
+
+# start development server
+npm run dev
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+
+      {/* Background Effects */}
+
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+      <main>
+        <div className="flex items-center justify-center min-h-screen flex-col">
+          <h1 className="text-5xl font-bold mb-4">Welcome to the Home Page</h1>
+          <p className="text-lg">
+            This is a React template using Vite and Tailwind CSS.
+          </p>
+        </div>
+      </main>
+
+      {/* Footer */}
+    </div>
+  );
+};
