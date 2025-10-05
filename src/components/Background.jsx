eb21c0730e76926193020afabbc82ddee3bd73cc
DoomLaptop <umutw4141@gmail.com>
2025-10-06 00:38:59 +0300

    Some changes.

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import Beams from './ui/Beams'
+import DarkVeil from './ui/DarkVeil'
+
 
 const Background = () => {
   return (
     <div className="fixed inset-0 w-full h-full -z-10">
-      <Beams
-        beamWidth={1.5}
-        beamHeight={15}
-        beamNumber={3}
-        lightColor="#fff"
-        speed={2}
-        noiseIntensity={2}
-        scale={0.2}
-        rotation={30}
-      />
+       <DarkVeil/>
     </div>
   )
 }
