9469a2ddfe5870216dd6f1cd6a9d436ff33fa00f
DoomLaptop <umutw4141@gmail.com>
2025-10-06 15:25:19 +0300

    Some changes.

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import DarkVeil from './ui/DarkVeil'
+import PixelBlast from './ui/PixelBlast'
 
 
 const Background = () => {
   return (
-    <div className="fixed inset-0 w-full h-full -z-10">
-       <DarkVeil/>
+    <div className="fixed inset-0 w-full h-full -z-10 bg-black">
+       <PixelBlast
+        variant="diamond"
+        pixelSize={6}
+        color="#7f1d1d"
+        patternScale={2}
+        patternDensity={1}
+        pixelSizeJitter={0}
+        enableRipples
+        rippleSpeed={0.5}
+        rippleThickness={0.12}
+        rippleIntensityScale={1.5}
+        liquidStrength={0.12}
+        liquidRadius={1.2}
+        liquidWobbleSpeed={5}
+        speed={0.5}
+        edgeFade={0.25}
+  />
     </div>
   )
 }
