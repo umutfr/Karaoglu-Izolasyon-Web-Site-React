7e5b7a5bd3d4ffc8ef8e0f9b89254852140e4ca0
DoomLaptop <umutw4141@gmail.com>
2025-10-06 16:02:46 +0300

    Some changes.

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import ElectricBorder from './ui/ElectricBorder';
+import React from "react";
+import ElectricBorder from "./ui/ElectricBorder";
+import { useMediaQuery } from "react-responsive";
+import clsx from "clsx";
 
 const ServiceCard = ({ icon, title, description, image }) => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  // Mobile'da normal div, değilse ElectricBorder
+  const Wrapper = isMobile ? "div" : ElectricBorder;
+
   return (
-    <ElectricBorder
+    <Wrapper
       color="oklch(39.6% 0.141 25.723)"
       speed={1}
       chaos={0.3}
       thickness={4}
-      className="
-        bg-[black] rounded-xl
-        shadow-md p-4 md:p-3 w-80 
-        hover:shadow-lg hover:scale-105 
-        transition-all duration-300 cursor-pointer 
-        md:h-[250px] md:w-[500px] 
-        flex flex-col items-center md:gap-4 
-        md:text-center md:justify-center md:pb-9
-      "
+      className={clsx(
+        "bg-[black] rounded-xl shadow-md p-4 md:p-3 w-80 hover:shadow-lg hover:scale-105 transition-all duration-300 cursor-pointer md:h-[250px] md:w-[500px] flex flex-col items-center md:gap-4 md:text-center md:justify-center md:pb-9",
+        isMobile && "border-2 border-red-900" // sadece mobile için border ekle
+      )}
     >
       {image && (
         <img
-          loading='lazy'
+          loading="lazy"
           src={image}
           alt={title}
           className="w-full h-40 object-cover rounded-lg mb-4"
@@ -34,10 +36,8 @@ const ServiceCard = ({ icon, title, description, image }) => {
         </h3>
       </div>
 
-      <p className="text-sm text-white leading-snug md:text-lg">
-        {description}
-      </p>
-    </ElectricBorder>
+      <p className="text-sm text-white leading-snug md:text-lg">{description}</p>
+    </Wrapper>
   );
 };
 
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ElectricBorder from './ui/ElectricBorder';
+import { useMediaQuery } from "react-responsive";
+import clsx from "clsx";
+
 
 const TestimonialCard = ({ image, name, rating, text, }) => {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  const Wrapper = isMobile ? "div" : ElectricBorder;
   return (
-    <ElectricBorder
+    <Wrapper
      color="oklch(39.6% 0.141 25.723)"
   speed={1}
   chaos={0.5}
@@ -11,7 +17,7 @@ const TestimonialCard = ({ image, name, rating, text, }) => {
   className="p-3 rounded-3xl">
 
 
-    <div className="flex flex-col justify-center items-center text-center bg-black/70 border-[3px] border-white/20 rounded-3xl md:min-h-[550px] md:max-w-[450px] px-12 py-12 cursor-pointer transition-all duration-300 hover:border-red-900 hover:shadow-[0_0_50px_rgba(158,0,0,1)] max-h-[600px] md:gap-8 gap-6">
+    <div className={clsx("flex flex-col justify-center items-center text-center bg-black/70 rounded-3xl md:min-h-[550px] md:max-w-[450px] px-12 py-12 cursor-pointer transition-all duration-300 hover:border-red-900 hover:shadow-[0_0_50px_rgba(158,0,0,1)] max-h-[600px] md:gap-8 gap-6", isMobile && "border-[3px] border-red-900")}>
     <ElectricBorder
      color="oklch(39.6% 0.141 25.723)"
   speed={1}
@@ -35,7 +41,7 @@ const TestimonialCard = ({ image, name, rating, text, }) => {
   </div>
   <p className="text-md leading-relaxed">{text}</p>
 </div>
-    </ElectricBorder>
+    </Wrapper>
 
   );
 };
